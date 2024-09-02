1350b5f680d09b07e118a98b6d2dfd6abeff1603
AS-Guard <AS-Guard@mail.ru>
2024-09-02 21:12:15 +0500

    config files on 02-09-2024 21:12:15

--- ha_gitpush.sh
+++ ha_gitpush.sh
@@ -4,9 +4,10 @@ cd /config
 #Add all files to the repository with respect to .gitignore rules 
 git add README
 git add .gitignore
+git add ha_gitpush.sh
 git add automations.yaml
 git add configuration.yaml
-git add includes/**/*.yaml
+git add -f includes/**/*.yaml
 #Commit changes with message with current date stamp
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
 #Push changes towards GitHub
